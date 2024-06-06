9f94c36733b67080c96f15ca56e886807bcb0842
Kahtzehnn MAC <marki0172633@gmail.com>
2024-06-06 15:39:15 +1000

    minor

--- npalib/render.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class InputLayer:
-    def __init__(self, numInputs, colour): #add more params later
-        self.numInputs = numInputs
-        self.colour = colour
-        for i in range(self.numInputs):
-            pygame.draw
