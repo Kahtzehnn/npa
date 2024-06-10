9401272fa680f564429b98f3efe57958c50a1088
Kahtzehnn <0172633@schoolsnet.act.edu.au>
2024-06-10 18:23:38 +1000

    Minor

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import pygame
+import numpy as np
 
-def init():
-    pygame.init()
-    pygame.time.Clock.tick(20)
-    screen = pygame.display.set_mode((1600, 900))
-    pygame.display.set_caption("NPA")
-    running = True
+pygame.init()
+WIDTH, HEIGHT = 1700, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("NPA")
+
+def setLayers():
+    screen.fill((100, 100, 100))
+    inputColor = (220, 220, 100)
+    interColor = (180, 180, 180)
+    LayerGap = 80
+    NeuronGap = WIDTH//10 #CHANGE TO number of neurons per layer
+
+    #INPUTS
+    for j in range(10): #CHANGE TO number of neurons per layer
+        pygame.draw.circle(screen, inputColor, (100+LayerGap,(NeuronGap+j*NeuronGap)),10)
+
+    #INTERNEURONS
+    for i in range(10):
+        for j in range(5):
+            pygame.draw.circle(screen, interColor, (300 +i*40, (50 + j*40)), 6, 3)
+
+    #OUTPUTS
+    for i in range(10):
+        pygame.draw.circle(screen, interColor, (300 +i*40, (50 + i*40)), 15, 4)
+
+
+
+
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    setLayers()
+    pygame.display.update()
