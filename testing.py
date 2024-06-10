5fec5342de1ab3dee35f121d89cf755c4c736962
Kahtzehnn <168258881+Kahtzehnn@users.noreply.github.com>
2024-06-10 23:00:08 +1000

    added nec files

---
+++ testing.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy as sci
+
+### CODE SNIPPETS ###
+
+
+
+#####################
+
+inputs = np.array([0,0,0,0,0])
+
+inters = np.array([
+    [0,0,0,0,0,0,7,0,0,0], 
+    [0,0,0,4,0,0,0,0,0,0], 
+    [0,0,3,0,0,0,0,0,0,0]
+])
+
+outputs = np.array([0,0,0,0,0])
+
+
+
